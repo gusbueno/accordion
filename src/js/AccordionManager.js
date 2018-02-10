498b8cb705a539bc12fc4e6acde432c815fbba94
Gustavo Bueno <gustavo.bueno89@gmail.com>
2018-02-10 19:16:44 +0100

    remove console.log

--- src/js/AccordionManager.js
+++ src/js/AccordionManager.js
@@ -2,7 +2,6 @@ class AccordionManager {
     constructor(containerName) {
         this.accordionHeaders = [];
         this.accordionHeaders.push.apply(this.accordionHeaders, document.getElementById(containerName).getElementsByClassName(`${containerName}-header`));
-        console.log(this.accordionHeaders);
         this.addOnclickEventToHeaders();
     }
 
@@ -24,7 +23,6 @@ class AccordionManager {
                     xhr.onreadystatechange = function () {
                         if (xhr.readyState === 4) { // readyState 4 means the request is done.
                             if (xhr.status === 200) { // status 200 is a successful return.
-                                console.log(xhr.responseText); // 'This is the returned text.'
                                 const jsonData = JSON.parse(xhr.responseText);
                                 textElement.innerHTML = `The temperature on ${jsonData.title} is ${jsonData.temp}º`;
                             }
